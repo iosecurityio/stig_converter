350b236af3d7e165c7344730ec65cc47cd5e8197
Allen Montgomery <15912502+iosecurityio@users.noreply.github.com>
2022-03-27 16:58:36 -0400

    now writes CSV, need to understand weird format in some cases

--- ckl_to_splunk.py
+++ ckl_to_splunk.py
@@ -1,14 +1,18 @@
 import csv
-import requests
-import xml.etree.ElementTree as ET
+import glob
+from importlib import reload
 import os
+import requests
+import sys
+import xml.etree.ElementTree as ET
 
-# shared drive location - string
-SHARE = "//bamtech/public/IA/Projects"
-#projects in the projects folder - list
-PROJECTS = ["/afaems","/slcms","/myvector","/seco","/msep","/aiportal"]
-#location of checklist - string
-CHECKLIST = "F:/Allen/python/STIGs/blank_checklist.ckl"
+
+#SHARE = "//bamtech/public/IA/Projects"
+#PROJECTS = ["/afaems","/slcms","/myvector","/seco","/msep","/aiportal"]
+
+INPUT_CKL = "F:/Allen/python/ckl_to_csv/blank_checklist.ckl"
+OUTPUT_CSV = "F:/Allen/python/ckl_to_csv/TestNewChecklist.csv"
+CKL_GLOB = "F:/Allen/python/ckl_to_csv/*.ckl"
 
 def check_connection(url, timeout=5):
     try:
@@ -19,45 +23,28 @@ def check_connection(url, timeout=5):
     return False
 
 
-def parse_XML(cklfile=CHECKLIST):
+def parse_XML(input_file=INPUT_CKL):
+    """
     # create element tree object using xml etrees parse function
     tree = ET.parse(cklfile)
     # get root element (ie <CHECKLIST> on line 3)
     root = tree.getroot()
-    # create empty lists for the asset and findings   
-    findings = []
     # Parse Asset Information
+    
     for node in root.findall('./ASSET'):
         # empty asset dictionary
         asset = {}
-        # iterate child elements of <ASSET> 
+        # iterate child elements of <ASSET> and put relevant info in the list
         for subnode in node:
             if subnode.tag == "ROLE":
-                asset['role'] = subnode.text.encode('utf8')
+                asset['role'] = subnode.text
             elif subnode.tag == "ASSET_TYPE":
-                asset['asset_type'] = subnode.text.encode('utf8')
+                asset['asset_type'] = subnode.text
             elif subnode.tag == "MARKING":
-                asset['marking'] = subnode.text.encode('utf8')
+                asset['marking'] = subnode.text
             elif subnode.tag == "HOST_NAME":
-                asset['host_name'] = subnode.text.encode('utf8')
-            elif subnode.tag == "HOST_IP":
-                asset['host_ip'] = subnode.text.encode('utf8')
-            elif subnode.tag == "HOST_MAC":
-                asset['host_mac'] = subnode.text.encode('utf8')
-            elif subnode.tag == "HOST_FQDN":
-                asset['host_fqdn'] = subnode.text.encode('utf8')
-            elif subnode.tag == "TARGET_COMMENT":
-                asset['target_comment'] = subnode.text.encode('utf8')
-            elif subnode.tag == "TECH_AREA":
-                asset['tech_area'] = subnode.text.encode('utf8')                
-            elif subnode.tag == "TARGET_KEY":
-                asset['target_key'] = subnode.text.encode('utf8')
-            elif subnode.tag == "WEB_OR_DATABASE":
-                asset['web_or_database'] = subnode.text.encode('utf8')
-            elif subnode.tag == "WEB_DB_SITE":
-                asset['web_db_site'] = subnode.text.encode('utf8')
-            elif subnode.tag == "WEB_DB_INSTANCE":
-                asset['web_db_instance'] = subnode.text.encode('utf8')
+                asset['host_name'] = subnode.text
+
             else:
                 continue
         # Add everything to the asset list
@@ -75,42 +62,55 @@ def parse_XML(cklfile=CHECKLIST):
             if child.tag == '{http://search.yahoo.com/mrss/}content':
                 findings['media'] = child.attrib['url']
             else:
-                findings[child.tag] = child.text.encode('utf8')
+                findings[child.tag] = child.text
   
         # append finding dictionary to findings list
         finding_list.append(findings)
       
     # return findings list
     return finding_list
-  
-  
-def savetoCSV(newsitems, filename):
-    # specifying the fields for csv file
-    fields = ['id','severity','title','description','iacontrols','ruleID','fixid','fixtext','checkid','checktext']
-    
+    """
+    return 1
+
+
+def savetoCSV(input_file=INPUT_CKL,output_file=OUTPUT_CSV):
+
+    fieldnames = ['HOST_NAME', 'HOST_IP', 'Vuln_Num', 'Severity', 'Group_Title', 'Rule_ID', 'Rule_Ver', 'Rule_Title', 'Fix_Text']
+
     # writing to csv file
-    with open(filename, 'w') as csvfile:
-  
-        # creating a csv dict writer object
-        writer = csv.DictWriter(csvfile, fieldnames = fields)
-  
-        # writing headers (field names)
+    with open(output_file, 'w', newline='') as csvfile:
+        # specifying the fields for csv file
+        # fields = ['id','severity','title','description','iacontrols','ruleID','fixid','fixtext','checkid','checktext']
+        # creating a csv DictWriter object to write to
+        writer = csv.DictWriter(csvfile, delimiter=',', fieldnames = fieldnames)
+
+        # writing headers aka field names
         writer.writeheader()
   
-        # writing data rows
-        writer.writerows(newsitems)
-  
+        for checklist in glob.glob(CKL_GLOB):
+                print("Processing: " + checklist)
+                tree = ET.parse(checklist)
+                root = tree.getroot()
+                finding = {}
+
+                for asset in root.iter('ASSET'):
+                    finding['HOST_NAME'] = asset.find('HOST_NAME').text
+                    finding['HOST_IP'] = asset.find('HOST_IP').text
+
+                for vuln in root.iter('VULN'):
+                    for stig_data in vuln.findall('./STIG_DATA'):
+                        if (stig_data.find('VULN_ATTRIBUTE').text in ['Vuln_Num', 'Severity', 'Group_Title', 'Rule_ID', 'Rule_Ver', 'Rule_Title', 'Fix_Text']):
+                            finding[stig_data.find('VULN_ATTRIBUTE').text] = stig_data.find('ATTRIBUTE_DATA').text
+        
+                    writer.writerow(finding)
+
       
 def main():
-    #check access to shared drive
+
     #check_connection(SHARE)
+    savetoCSV()
 
-    # parse xml file
-    newsitems = parse_XML()
 
-    # store news items in a csv file
-    #savetoCSV(newsitems, 'topnews.csv')
-
-      
+# RUN RUN RUN      
 if __name__ == "__main__":
     main()
---
+++ gistfile1.py
@@ -0,0 +1,39 @@
+###
+# A Python script to extract data out of a DISA STIG Viewer xccdf file to a CSV
+# @author Michael Joseph Walsh <github.com@nemonik.com>
+##
+import csv
+import glob
+import os
+import sys
+import xml.etree.ElementTree as ET
+
+# Original github here:
+# https://gist.github.com/nemonik/951a0e55436e0708222b
+
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+with open('tmp.csv', 'ab+') as csvfile:
+        fieldnames = ['HOST_NAME', 'HOST_IP', 'Vuln_Num', 'Severity', 'Group_Title', 'Rule_ID', 'Rule_Ver', 'Rule_Title', 'Fix_Text']
+        output = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        output.writeheader()
+
+        for filename in glob.glob("*.xml"):
+                print"processing: " + filename
+                tree = ET.parse(filename)
+                root = tree.getroot()
+                row = {}
+
+                for asset in root.iter('ASSET'):
+                        row['HOST_NAME'] = asset.find('HOST_NAME').text
+                        row['HOST_IP'] = asset.find('HOST_IP').text
+
+                for vuln in root.iter('VULN'):
+                        for stig_data in vuln.findall('STIG_DATA'):
+                                if (stig_data.find('VULN_ATTRIBUTE').text in ['Vuln_Num', 'Severity', 'Group_Title', 'Rule_ID', 'Rule_Ver', 'Rule_Title', 'Fix_Text']):
+                                        row[stig_data.find('VULN_ATTRIBUTE').text] = stig_data.find('ATTRIBUTE_DATA').text
+
+                output.writerow(row)
