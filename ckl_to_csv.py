88ab92c06fb6e8a92a91a05a203cca893c3104b7
Allen Montgomery <15912502+iosecurityio@users.noreply.github.com>
2022-03-27 19:59:03 -0400

    more updates to ckl_to_csv

--- ckl_to_csv.py
+++ ckl_to_csv.py
@@ -1,16 +1,16 @@
 import csv
 import glob
-from importlib import reload
-import os
 import requests
 import xml.etree.ElementTree as ET
 
-# BAM Global vars
-#SHARE = "//bamtech/public/IA/Projects"
+# CKL/ckl always stands for STIG CHECKLIST, always.
+# GLOB is basically a directory path
 #PROJECTS = ["/afaems","/slcms","/myvector","/seco","/msep","/aiportal"]
-
+BAM_ROOT = "//bamtech/public/IA/Projects/"
+CKL_SHARE_GLOB = f"{BAM_ROOT}['afaems','slcms','myvector','seco','msep','aiportal']/STIGs/*.ckl"
 OUTPUT_CSV = "F:/Allen/python/ckl_to_csv/TestNewChecklist.csv"
 CKL_GLOB = "F:/Allen/python/ckl_to_csv/*.ckl"
+
 
 def check_connection(url, timeout=5):
     ## Check connection to the URL
@@ -22,21 +22,22 @@ def check_connection(url, timeout=5):
     return False
 
 
-def convert_to_csv(checklist_location=CKL_GLOB, output_file=OUTPUT_CSV):
-
+def convert_to_csv(checklists, output_file=OUTPUT_CSV):
+    ### Function that takes in a directory path, and converts .CKL to .CSV as an output_file(s)
+    # Define your table headers as fieldnames
     fieldnames = ['HOST_NAME', 'HOST_IP', 'Vuln_Num', 'Severity', 'STATUS', 'Group_Title', 'Rule_ID', 'Rule_Ver', 'Rule_Title', 'Fix_Text', 'FINDING_DETAILS', 'COMMENTS']
 
-    # Open a CSV file to write to
+    # Open a new CSV file to write to
     with open(output_file, 'w', newline='') as csvfile:
 
         # creates a CSV DictWriter to write dictionaries
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-        # writes fieldnames aka table headers if fieldnames is set
+        # writes fieldnames as the table headers
         writer.writeheader()
   
         # Grabs every checklist in the checklist_location
-        for checklist in glob.glob(checklist_location):
+        for checklist in checklists:
                 print("Processing: " + checklist)
                 
                 # Creates an XML elementtree from a checklist
@@ -68,11 +69,26 @@ def convert_to_csv(checklist_location=CKL_GLOB, output_file=OUTPUT_CSV):
                     writer.writerow(finding)
                     
                     
-      
+def get_checklists(checklist_location):
+    # just pass this a path and it will get checklists, regex syntax included
+    # returns list of every checklist detected in the path(s)
+    return [ckl for ckl in glob.glob(checklist_location, recursive=True)]
+
+
 def main():
 
-    #check_connection(SHARE)
-    convert_to_csv()
+    # 1. Check Connection at the beginning
+    # if check_connection(BAM_ROOT):
+
+    #2. Get list of checklists that exist, look at them
+    # get_checklists(location)
+    stig_checklists = get_checklists(CKL_GLOB)
+    print(f"Checklists detected: {stig_checklists}")
+
+    #3. convert them to csv files
+    # convert_to_csv(get_checklists(CKL_GLOB))
+    print(f"Converting {stig_checklists} to .csv")
+    convert_to_csv(stig_checklists, OUTPUT_CSV)
 
 
 # RUN RUN RUN      
