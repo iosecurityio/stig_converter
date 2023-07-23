2cb6852631034b0f5896ea87cacc3b061a6ed40d
a113n <15912502+iosecurityio@users.noreply.github.com>
2023-07-23 16:02:17 -0400

    first consolidation refactor complete

--- tests/stig_converter_test.py
+++ tests/stig_converter_test.py
@@ -5,7 +5,6 @@ Name: stig_converter_test.py
 import argparse
 import csv
 import json
-import os
 import re
 import sys
 import xml.etree.ElementTree as ET
@@ -16,15 +15,18 @@ from pathlib import Path
 class STIGConverter:
     """Converts STIG Checklists to/from various file formats (CSV, JSON, CKL)"""
 
-    def __init__(self, config) -> None:
-        self.config = config
-        self.input_file = config.input_file
-        self.output_file = config.output_file
-        self.events = False
+    def __init__(self, interface_opts) -> None:
+        self.config = interface_opts
+        self.input_file = interface_opts.input_file
+        self.output_file = interface_opts.output_file
+        self.events = False  # TODO: Add events to splunk
+        self.encoding = "utf-8"
+        self.date = datetime.now().strftime("%Y%m%d")
 
     def parse_hostname(self, checklist, project_list) -> str:
         """Takes a checklist location and a list of projects to look for in the path provided"""
 
+        # TODO refactor with pathlib
         project_path = checklist.split(r"/")
 
         # Check if the project is in the path and return empty string if not
@@ -34,46 +36,48 @@ class STIGConverter:
         return ""
 
     def update_filename(self, filename) -> str:
-        """Updates the timestamp of the checklist to the current date"""
+        """Updates the timestamp of the checklist to the current date
+        If the file already has a date in the filename, then it will replace it with the current date
+        If the file doesn't have a date in the filename, then it will append the current date to the end
+        Example: stig_checklist-20210723.ckl -> stig_checklist-20210724.ckl
+        Example: stig_checklist.ckl -> stig_checklist-20210724.ckl
+        """
 
+        # Check if the filename has a date in it (eg -20210723)
         pattern = r"-(\d{8})"
         match = re.search(pattern, filename)
-        current_date = datetime.now().strftime("%Y%m%d")
 
         if match:
             matched_sequence = match.group(1)
-            updated_filename = filename.replace(matched_sequence, current_date)
+            # replace the date in the filename with the current date
+            updated_filename = filename.replace(matched_sequence, self.date)
+            # return the name of the file with the updated date
             return updated_filename
         else:
+            # if there is no date in the filename, then add the current date to the end
             name, ext = filename.split(".")[:2]
-            updated_filename = f"{name}-{current_date}.{ext}"
+            updated_filename = f"{name}-{self.date}.{ext}"
             return updated_filename
 
-    def convert_json_to_ckl(self, json_data, ckl_path, base_ckl) -> str:
+    def convert_json_to_ckl(self, input_file, output_file):
         """Populates a STIG Checklist with the JSON equivalent values"""
 
-        # Read in the JSON Data
-        try:
-            with open(json_data, "r") as read_file:
-                loaded_data = json.load(read_file)
-        except FileNotFoundError as fnf:
-            print(f"[X] JSON didn't load properly: {fnf}")
-        except KeyError as ke:
-            print(f"[X] JSON didn't load properly: {ke}")
+        # We need a base checklist to populate with the JSON data
+        # TODO: handle the filename and path without relative path
+        base_ckl = "../data/stig_checklist.ckl"
 
         try:
+            # Read in the JSON Data
+            with open(input_file, "r") as read_file:
+                loaded_data = json.load(read_file)
             # Read in the STIG Checklist we are using as a template
             ckl_tree = ET.parse(base_ckl)
             ckl_root = ckl_tree.getroot()
-        except Exception as e:
-            print(f"[X] Invalid checklist {base_ckl}:\n\t{e}")
-
-        # List of elements under the Asset element in the XML structure
-        ASSET = ["HOST_NAME", "HOST_IP", "HOST_MAC", "HOST_FQDN", "TARGET_COMMENT"]
-        try:
+            # List of elements under the ASSET element in the CKL/XML structure
+            asset = ["HOST_NAME", "HOST_IP", "HOST_MAC", "HOST_FQDN", "TARGET_COMMENT"]
             for element in ckl_root.iter("ASSET"):
-                # Populate the element in the Asset section
-                if element.tag in ASSET:
+                # Populate the element in the ASSET section
+                if element.tag in asset:
                     element.text = loaded_data[0][element]
 
             # For every Vulnerability in the checklist
@@ -87,67 +91,60 @@ class STIGConverter:
                         ):
                             attribute_data = stig_data.find("ATTRIBUTE_DATA")
                             attribute_data.text = json_finding[stig_data.text]
+
+            # Turn the XML Root back into a string for writing
+            new_xml = ET.tostring(ckl_root, encoding=self.encoding)
+
+            # Write the modified XML to the new file
+            with open(output_file, "wb") as output_file:
+                # Custom header for xml declaration and STIG header comment
+                output_file.write(f'<?xml version="1.0" encoding="{self.encoding.upper()}"?>\n'.encode(self.encoding))
+                output_file.write("<!--DISA STIG Viewer :: 2.16-->\n".encode(self.encoding))
+                output_file.write(new_xml)
+
         except UnboundLocalError as unbound_error:
             print(f"[X] Error parsing {base_ckl}:\n\t{unbound_error}")
 
-        # Turn the XML Root back into a string for writing
-        encoding = "utf-8"
-        new_xml = ET.tostring(ckl_root, encoding=encoding)
+        except FileNotFoundError as fnf:
+            print(f"[X] JSON file not found: {fnf}")
 
-        # Write the modified XML to the new file
-        with open(ckl_path, "wb") as output_file:
-            # Custom header for xml declaration and STIG header comment
-            output_file.write('<?xml version="1.0" encoding="UTF-8"?>\n'.encode(encoding))
-            output_file.write("<!--DISA STIG Viewer :: 2.16-->\n".encode(encoding))
-            output_file.write(new_xml)
+        except KeyError as ke:
+            print(f"[X] JSON doesn't contain the expected key: {ke}")
 
-    def convert_csv_to_json(self, csv_file, json_path):
-        """Converts .csv to .json for Splunk events
-        :param csv_file: The location of the .ckl file to convert
-        :param json_path: The location path to write the .json file
-        :return: The absolute path of the new .json file
-        """
+        except Exception as e:
+            print(f"[X] Invalid checklist {base_ckl}:\n\t{e}")
+
+    def convert_csv_to_json(self, input_file, output_file, as_event=False):
+        """Converts .csv to .json file out or Splunk events"""
 
         # Create a list of findings
         json_array = []
 
         # Read in the CSV
-        with open(csv_file, encoding="utf-8") as csvf:
+        with open(input_file, encoding=self.encoding) as csvf:
             csv_reader = csv.DictReader(csvf)
-
             for row in csv_reader:
                 json_array.append(row)
 
-        # If you pass None as location, it will print to stdout for Splunk events
-        if json_path is None:
+        # TODO: Add events to splunk
+        # If events, it will print to stdout for Splunk events
+        if as_event:
             for entry in json_array:
                 print(entry)
+        # If not events, it will create a new file
         else:
-            filename = os.path.basename(csv_file)
-            new_name = os.path.splitext(filename)
-            new_location = f"{json_path}{new_name[0].replace(' ', '_')}-{DATE}.json"
             # Open the new file and dump the list of findings
-            with open(new_location, "w", encoding="utf-8") as jsonf:
+            with open(output_file, "w", encoding=self.encoding) as jsonf:
                 json_string = json.dumps(json_array, indent=4)
                 jsonf.write(json_string)
+        return output_file
 
-        return new_location
+    def convert_ckl_to_json(self, input_file, output_file, as_event=False):
+        """Converts a STIG Checklist .CKL file to .JSON file"""
 
-    def convert_ckl_to_json(ckl, json_path) -> str:
-        """Converts a STIG Checklist .CKL file to .JSON
-        :param ckl: The location of the .ckl file to convert
-        :param json_path: The location path to write the .json file
-        :return: The absolute path of the new .json file
-        """
-
-        filename = os.path.basename(ckl)
-        new_filename = os.path.splitext(filename)
-        # TODO Handle the / in the path
-        new_location = f"{json_path}/{new_filename[0].replace(' ', '_')}-{DATE}.json"
-
-        with open(new_location, "w", newline="", encoding="utf-8") as jsonf:
+        with open(output_file, "w", newline="", encoding=self.encoding) as jsonf:
             # Create an xml object from the ckl file
-            tree = ET.parse(ckl)
+            tree = ET.parse(input_file)
             root = tree.getroot()
             # Initialize hostname and IP before we parse checklist
             host_name = ""
@@ -165,10 +162,7 @@ class STIGConverter:
             # Parse the checklists individual Vulnerabilities
             for vuln in root.iter("VULN"):
                 # Create a finding dict to hold each finding info
-                finding = {}
-                finding["DATE"] = DATE
-                finding["HOST_NAME"] = host_name
-                finding["HOST_IP"] = host_ip
+                finding = {"HOST_NAME": host_name, "HOST_IP": host_ip}
                 for stig_data in vuln.findall("./STIG_DATA"):
                     if stig_data.find("VULN_ATTRIBUTE").text in [
                         "Vuln_Num",
@@ -185,15 +179,85 @@ class STIGConverter:
                 finding["STATUS"] = vuln.find("./STATUS").text
                 finding["FINDING_DETAILS"] = vuln.find("./FINDING_DETAILS").text
                 finding["COMMENTS"] = vuln.find("./COMMENTS").text
-                # Append a unique ID to map for each finding
-                finding["Unique_ID"] = f"{host_name}-{finding['Rule_ID']}-{DATE}"
                 # Add the finding to our finding list
                 findings.append(finding)
             # Dump the finding dictionary into the file we are creating
             json.dump(findings, jsonf, indent=4)
 
+        # TODO: Add events to splunk
+        # If events, it will print to stdout for Splunk events
+        if as_event:
+            for entry in findings:
+                print(entry)
+
         # Return the location of the new json doc
-        return new_location
+        return output_file
+
+    def convert_ckl_to_csv(self, input_file, output_file) -> str:
+        """Converts a CKL file to a CSV file"""
+
+        # CKL is a custom XML and these are the field names we will pull from the document
+        # Define your table headers as fieldnames
+        fieldnames = [
+            "DATE",
+            "HOST_NAME",
+            "HOST_IP",
+            "Vuln_Num",
+            "Severity",
+            "Group_Title",
+            "Rule_ID",
+            "Rule_Ver",
+            "Rule_Title",
+            "Fix_Text",
+            "STATUS",
+            "FINDING_DETAILS",
+            "COMMENTS",
+            "Unique_ID",
+        ]
+
+        with open(output_file, "w", newline="", encoding=self.encoding) as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            tree = ET.parse(input_file)
+            root = tree.getroot()
+
+            # create a dictionary to hold findings
+            finding = {
+                "DATE": datetime.now().strftime("%Y%m%d"),
+                "HOST_NAME": "",
+                "HOST_IP": "",
+            }
+            # Parse the checklist Asset details
+            for asset in root.iter("ASSET"):
+                if asset.find("HOST_NAME").text:
+                    finding["HOST_NAME"] = asset.find("HOST_NAME").text
+                if asset.find("HOST_IP").text:
+                    finding["HOST_IP"] = asset.find("HOST_IP").text
+
+            for vuln in root.iter("VULN"):
+                for stig_data in vuln.findall("./STIG_DATA"):
+                    if stig_data.find("VULN_ATTRIBUTE").text in [
+                        "Vuln_Num",
+                        "Severity",
+                        "Group_Title",
+                        "Rule_ID",
+                        "Rule_Ver",
+                        "Rule_Title",
+                        "Fix_Text",
+                    ]:
+                        finding[stig_data.find("VULN_ATTRIBUTE").text] = stig_data.find(
+                            "ATTRIBUTE_DATA"
+                        ).text.replace("\n", " ")
+
+                finding["STATUS"] = vuln.find("./STATUS").text
+                finding["FINDING_DETAILS"] = vuln.find("./FINDING_DETAILS").text
+                finding["COMMENTS"] = vuln.find("./COMMENTS").text
+                # Append a unique ID to map for each finding
+                finding["Unique_ID"] = f"{finding['HOST_NAME']}-{finding['Rule_ID']}-{finding['DATE']}"
+                # Write the finding to the CSV
+                writer.writerow(finding)
+        # Return the location of the new CSV
+        return output_file
 
 
 class Interface:
@@ -261,80 +325,6 @@ class Interface:
             print(f"[-] Error: {e}")
             sys.exit(1)
 
-    def convert_ckl_to_csv(ckl, csv_path) -> str:
-        """Converts a CKL file to a CSV file
-        :param ckl: The location of the .ckl file to convert
-        :param csv_path: The location to write the .csv file
-        :return: The location of the new .csv file
-        """
-
-        # CKL is a custom XML and these are the field names we will pull from the document
-        # Define your table headers as fieldnames
-        fieldnames = [
-            "DATE",
-            "HOST_NAME",
-            "HOST_IP",
-            "Vuln_Num",
-            "Severity",
-            "Group_Title",
-            "Rule_ID",
-            "Rule_Ver",
-            "Rule_Title",
-            "Fix_Text",
-            "STATUS",
-            "FINDING_DETAILS",
-            "COMMENTS",
-            "Unique_ID",
-        ]
-        current_date = datetime.now().strftime('%Y%m%d')
-        filename = os.path.basename(ckl)
-        new_filename = os.path.splitext(filename)
-        new_csv = f"{csv_path}{new_filename[0].replace(' ', '_')}-{current_date}.csv"
-
-        with open(new_csv, "w", newline="", encoding="utf-8") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            tree = ET.parse(ckl)
-            root = tree.getroot()
-
-            # create a dictionary to hold findings
-            finding = {
-                "DATE": current_date,
-                "HOST_NAME": "",
-                "HOST_IP": "",
-            }
-            # Parse the checklist Asset details
-            for asset in root.iter("ASSET"):
-                if asset.find("HOST_NAME").text:
-                    finding["HOST_NAME"] = asset.find("HOST_NAME").text
-                if asset.find("HOST_IP").text:
-                    finding["HOST_IP"] = asset.find("HOST_IP").text
-
-            for vuln in root.iter("VULN"):
-                for stig_data in vuln.findall("./STIG_DATA"):
-                    if stig_data.find("VULN_ATTRIBUTE").text in [
-                        "Vuln_Num",
-                        "Severity",
-                        "Group_Title",
-                        "Rule_ID",
-                        "Rule_Ver",
-                        "Rule_Title",
-                        "Fix_Text",
-                    ]:
-                        finding[stig_data.find("VULN_ATTRIBUTE").text] = stig_data.find(
-                            "ATTRIBUTE_DATA"
-                        ).text.replace("\n", " ")
-
-                finding["STATUS"] = vuln.find("./STATUS").text
-                finding["FINDING_DETAILS"] = vuln.find("./FINDING_DETAILS").text
-                finding["COMMENTS"] = vuln.find("./COMMENTS").text
-                # Append a unique ID to map for each finding
-                finding["Unique_ID"] = f"{finding['HOST_NAME']}-{finding['Rule_ID']}-{current_date}"
-                # Write the finding to the CSV
-                writer.writerow(finding)
-        # Return the location of the new CSV
-        return new_csv
-
     def validate_file(self, stig_file):
         # Validate input file and output file
 
@@ -377,11 +367,15 @@ class Interface:
 def main():
     """Main function to run the script"""
 
-    # python stig_converter.py -i "../data/stig_checklist.ckl" -o "../data.json"
-    config = Interface()
-    converter = STIGConverter(config=config)
-    converter.convert()
-
+    try:
+        # python stig_converter.py -i "../data/stig_checklist.ckl" -o "../data.json"
+        config = Interface()
+        converter = STIGConverter(config)
+        # Run the conversion
+        print("Run conversion now")
+    except Exception as e:
+        print("[-] Error: Unable to run STIG conversion [-]")
+        print(e)
 
 if __name__ == "__main__":
     main()
